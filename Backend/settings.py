d33bd2536a3d3bcfd1d345dc9155bc314e406e84
Pritam <panchalpritam105@gmail.com>
2025-06-30 14:28:24 +0530

    new

--- Backend/settings.py
+++ Backend/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
